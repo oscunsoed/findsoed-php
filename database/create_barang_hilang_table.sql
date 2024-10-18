d31f3bdb79cc41b9e47597fd1e6aff6323b20b07
piajar <azharsuadmaja123@gmail.com>
2024-10-18 20:10:51 +0700

    test upload

--- database/create_barang_hilang_table.sql
+++ database/create_barang_hilang_table.sql
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 -- Active: 1728649722502@@127.0.0.1@3306@findsoed
+=======
+-- Active: 1728649750395@@127.0.0.1@3306@findsoed
+>>>>>>> ab520ae (test upload)
 CREATE TABLE barang_hilang (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nama VARCHAR(255) NOT NULL,
